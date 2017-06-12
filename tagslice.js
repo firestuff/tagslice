c55664813f0e3d9cfe1e0c4533bdf63acf648d89
Ian Gulliver <flamingcow@gmail.com>
2017-06-12 20:08:32 +0000

    More style, create dom, better class structure

--- tagslice.js
+++ tagslice.js
@@ -2,12 +2,33 @@
 
 class TagSlice {
 	constructor(container) {
+		this.buildDom_(container);
+		this.loadJson_();
+	}
+
+	buildDom_(container) {
+		let outer = document.createElement('outer');
+		container.appendChild(outer);
+
+		let taglist = document.createElement('taglist');
+		outer.appendChild(taglist);
+		taglist.innerText = 'tag!';
+
+		let objectlist = document.createElement('objectlist');
+		outer.appendChild(objectlist);
+	}
+
+	loadJson_() {
 		let xhr = new XMLHttpRequest();
 		xhr.open('GET', 'tagslice.json');
 		xhr.responseType = 'json';
 		xhr.onload = () => {
-			console.log(xhr.response);
+			this.onJsonLoad_(xhr.response);
 		};
 		xhr.send();
+	}
+
+	onJsonLoad_(response) {
+		console.log('foo', response);
 	}
 }
