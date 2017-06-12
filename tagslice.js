d34120be72a8227522e86b20a894fd99159c9429
Ian Gulliver <flamingcow@gmail.com>
2017-06-12 06:01:58 +0000

    Fetch objects file

--- tagslice.js
+++ tagslice.js
@@ -2,6 +2,12 @@
 
 class TagSlice {
 	constructor(container) {
-		container.innerText = 'test';
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', 'tagslice.json');
+		xhr.responseType = 'json';
+		xhr.onload = () => {
+			console.log(xhr.response);
+		};
+		xhr.send();
 	}
 }
